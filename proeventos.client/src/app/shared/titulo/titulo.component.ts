b6038bea9392a50c8caa37209d8e65142482c1b4
mateus-jaszumbek <mateusjaszumbek@hotmail.com>
2024-09-28 17:41:51 -0300

    deploy Mateus 28-09-2024

--- proeventos.client/src/app/shared/titulo/titulo.component.ts
+++ proeventos.client/src/app/shared/titulo/titulo.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TituloComponent implements OnInit {
 
-   @Input() titulo: string = 'ProEventos';
+
+  @Input() titulo!: string;
+  @Input() iconClass: string = 'fa fa-user';
+  @Input() subtitulo: string = 'Desde 2023';
 
   constructor() { }
 
